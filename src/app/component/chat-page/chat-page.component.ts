4a28bac84180be8f349ca1dbb65e438b589ab1c0
Mikita <73610514+MikitaSerou@users.noreply.github.com>
2022-04-13 16:14:51 +0300

    smooth scrolling message space

--- src/app/component/chat-page/chat-page.component.ts
+++ src/app/component/chat-page/chat-page.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  AfterViewChecked,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild
+} from "@angular/core";
 
 @Component({
   selector: "app-chat-page",
   templateUrl: "./chat-page.component.html",
   styleUrls: ["./chat-page.component.sass"]
 })
-export class ChatPageComponent implements OnInit {
+export class ChatPageComponent implements OnInit, AfterViewChecked {
   username: string | undefined | null =
     window.sessionStorage.getItem("username");
+
+  @ViewChild("scrollMe") private myScrollContainer: ElementRef;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom(): void {
+    try {
+      this.myScrollContainer.nativeElement.scrollTop =
+        this.myScrollContainer.nativeElement.scrollHeight;
+    } catch (err) {}
+  }
 }
